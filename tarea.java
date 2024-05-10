752487ad6a9f473856671dbfd0799f05f157332a
Fernanda26 <mfalvarezm@eafit.edu.co>
2024-05-10 16:11:14 -0500

    añadí archivo que imprime mi nombre

---
+++ tarea.java
@@ -0,0 +1,13 @@
+public class tarea {
+    /**
+     * Prints the phrase "Assignment 3 by <your full name>"
+     */
+    //HOLA, ESTOS SON LOS CAMBIOS <3
+    public static void printTarea() {
+        System.out.println("Assignment 3 by Maria Fernanda Alvarez Marin");
+    }
+
+    public static void main(String[] args) {
+        printTarea();
+    }
+}
